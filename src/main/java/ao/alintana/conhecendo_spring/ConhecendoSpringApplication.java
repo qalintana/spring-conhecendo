5c19a6c5283a5b355b5009b3f4ffe3d0dee5472d
Queiroz Santana <alintana18@gmail.com>
2024-09-11 16:58:30 +0100

    primeiro commit

---
+++ src/main/java/ao/alintana/conhecendo_spring/ConhecendoSpringApplication.java
@@ -0,0 +1,13 @@
+package ao.alintana.conhecendo_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConhecendoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConhecendoSpringApplication.class, args);
+	}
+
+}
